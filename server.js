40ca4b4012496bce75dd02e2a532eebe4fc61a91
Iwona Maraszkiewicz <ikonopinska@gmail.com>
2021-10-13 21:22:17 +0200

    Add finish project

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express');
-
+const socket = require('socket.io');
 const app = express();
+
+let tasks = [];
+
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running...');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  socket.emit('updateData', tasks);
+  socket.on('addTask', (task) => {
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task);
+  });
+
+  socket.on('removeTask', (id) => {
+    tasks.filter(task => task.id !== id);
+    socket.broadcast.emit('removeTask', id);
+  });
 });
 
 app.use((req, res) => {
